a3ec6d849bd43ba74eff3d4e101c589157516c64
emilyzimmerman <zimmermanemily121@gmail.com>
2022-08-25 17:59:15 -0500

    Correct answer to warm up

--- warmUp.js
+++ warmUp.js
@@ -1,11 +1,23 @@
-function findWaldo(group) {
-  let waldoArray = Array.prototype.filter(string => "Waldo")
 
-  if (waldoArray === true){
-    let foundWaldo = text.replace("Waldo", "Found Waldo")
+//function findWaldo(group) {
+  //let waldoArray = Array.prototype.find(string = "Waldo")
 
-    return waldoArray
-  };
+  //if (waldoArray === true){
+    //let foundWaldo = text.replace("Waldo", "Found Waldo")
+
+    //return waldoArray
+  //};
+//}
+
+//console.log(findWaldo(['Stacy', 'Waldo', 'John']))
+
+function findWaldo(group){
+
+  for(let i = 0; i < group.length; i++){
+    console.log(group[i])
+  }
+
 }
 
-console.log(findWaldo(['Stacy', 'Waldo', 'John']))
+findWaldo(['Stacy', 'Waldo', 'John']) 
+findWaldo(['Waldo', 'Jimmy', 'James', 'Jamie', 'Waldo']) 
